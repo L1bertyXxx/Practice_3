07fe89c2428b24ad4db13108f4aced832a039d0d
L1berty <larsanik92@gmail.com>
2025-03-02 19:35:06 +0300

    task4

---
+++ Task_4.py
@@ -0,0 +1,9 @@
+import random
+
+a = random.randint(1,100)
+b = random.randint(1,100)
+print(f"a = {a}")
+print(f"b = {b}")
+c = float(input("Введите с:  "))
+sR = (a+b+c)/3
+print(f"Cреднее арифметическое равно {sR}")
